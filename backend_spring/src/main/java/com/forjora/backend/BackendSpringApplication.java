129cb43b241b71a4e4ee8471221c06cfd5c18597
lakrasumit <lakrasumit633@gmail.com>
2026-09-10 23:32:14 +0530

    spring-boot initial setup

---
+++ backend_spring/src/main/java/com/forjora/backend/BackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.forjora.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringApplication.class, args);
+    }
+
+}
